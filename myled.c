27c77a27a68e3c19be119b7d5aed0a90dc50b1e1
Kristian Hartikainen <kristian@statwing.com>
2015-02-25 14:06:55 +0200

    jiiri

--- myled.c
+++ myled.c
@@ -33,15 +33,15 @@ void reset_timer(void) {
 
 void setup_timer(void) {
   // TCCR1B Timer/Counter 1 control register B
-  TCCR1B |= (1 << WGM12); // use hw timer comparison
+  TCCR2B |= (1 << WGM22); // use hw timer comparison
   // TIMSK1 Timer/Counter 1 Interrupt Mask Register
   // TIMER1_COMPA
-  TIMSK1 |= (1 << OCIE1A);  // enable ctc interrupt
+  TIMSK2 |= (1 << OCIE2A);  // enable ctc interrupt
   // http://www.gnu.org/savannah-checkouts/non-gnu/avr-libc/user-manual/group__avr__interrupts.html#ogaad5ebd34cb344c26ac87594f79b06b73
   sei();
   // Define the address for the comparison value
-  OCR1A = 49999;
-  TCCR1B |= ((1 << CS10) | (1 << CS11)); // timer 1 control register B w prescaler
+  OCR2A = 120;
+  TCCR2B |= ((1 << CS20) | (1 << CS21)); // timer 1 control register B w prescaler
 }
 
 uint8_t check_switch_state();
@@ -52,7 +52,7 @@ int main(void) {
   setup_ddr();
   setup_timer();
   LEDS_PORT = 0;
-  int btn_pressed;
+  int buttons_down;
 
   for(;;) {
     buttons_down = ~SWITCH_PIN;
@@ -71,7 +71,7 @@ uint8_t check_switch_state() {
 }
 
 // Setting TIMSK1 to what it is sets a listener for TIMER1_COMPA_vect
-ISR(TIMER1_COMPA_vect) {
+ISR(TIMER2_COMPA_vect) {
   LEDS_PORT = 0b00000000;
   buttons_ready = 1;
   // TODO: this is called when the the timer counter hits the value 49999
